a18860776a4c8cd6a0a21b08c25b10cbdce56c09
Jack Moroney <moroney.jo@gmail.com>
2025-08-19 15:02:55 +0100

    adding new files

---
+++ FAQs/index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trumpo</title>
+
+    <?php
+    include("../links.html");
+    ?>
+
+</head>
+
+
+
+<body>
+    <?php
+    include("../header.php");
+    ?>
+
+    <div class="container mt-5 pt-5">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h1>FAQs</h1>
+                <p>Find answers to common questions about Trumpo.com.</p>
+            </div>
+
+            <div class="row">
+                <div class="col-md-4 mb-4">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">How do BIPS work?</h2>
+                            <p class="card-text"></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-4">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">Is Trumpo.com regulated?</h2>
+                            <p class="card-text"></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-4">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">Can I track my referrals?</h2>
+                            <p class="card-text"></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-4 mb-4">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">Do I need to be a business to join?</h2>
+                            <p class="card-text"></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-4">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">How is Bitcoin used in your model?</h2>
+                            <p class="card-text"></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <?php
+        include("../footer.php");
+        ?>
+
+
+
+</body>
+
+</html>
